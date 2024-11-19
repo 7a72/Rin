61b45805206a78063ddb127ad9242b4c5ac1cdc1
c3bf26 <x@zrlab.org>
2024-11-19 13:26:49 +0800

    misc: up sql

--- server/sql/0000.sql
+++ server/sql/0000.sql
@@ -9,15 +9,6 @@ CREATE TABLE IF NOT EXISTS `comments` (
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS `feed_hashtags` (
-	`feed_id` integer NOT NULL,
-	`hashtag_id` integer NOT NULL,
-	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
-	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
-	FOREIGN KEY (`feed_id`) REFERENCES `feeds`(`id`) ON UPDATE no action ON DELETE cascade,
-	FOREIGN KEY (`hashtag_id`) REFERENCES `hashtags`(`id`) ON UPDATE no action ON DELETE cascade
-);
---> statement-breakpoint
 CREATE TABLE IF NOT EXISTS `feeds` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`alias` text,
@@ -49,13 +40,6 @@ CREATE TABLE IF NOT EXISTS `friends` (
 	FOREIGN KEY (`uid`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS `hashtags` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
-	`updated_at` integer DEFAULT (unixepoch()) NOT NULL
-);
---> statement-breakpoint
 CREATE TABLE IF NOT EXISTS `users` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`username` text NOT NULL,
@@ -65,3 +49,29 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
 	`updated_at` integer DEFAULT (unixepoch()) NOT NULL
 );
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS `metas` (
+    `id` integer PRIMARY KEY NOT NULL,
+    `name` text NOT NULL,
+    `alias` text,
+    `type` text NOT NULL,
+    `description` text,
+    `parent` integer,
+    `created_at` integer DEFAULT (unixepoch()) NOT NULL,
+    `updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+    FOREIGN KEY (`parent`) REFERENCES `metas`(`id`) ON UPDATE no action ON DELETE set null
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS `feed_metas` (
+    `feed_id` integer NOT NULL,
+    `meta_id` integer NOT NULL,
+    `type` text NOT NULL,
+    `created_at` integer DEFAULT (unixepoch()) NOT NULL,
+    `updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+    FOREIGN KEY (`feed_id`) REFERENCES `feeds`(`id`) ON UPDATE no action ON DELETE cascade,
+    FOREIGN KEY (`meta_id`) REFERENCES `metas`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE INDEX idx_metas_type ON metas(type);
+CREATE INDEX idx_metas_alias ON metas(alias);
+CREATE INDEX idx_feed_metas_type ON feed_metas(type);
